1ee566e9b3a2e8113fec6a1749a77e8130247e4e
Kyle  Beckstead <kyle21beckstead@gmail.com>
2019-09-25 22:01:52 +0000

    Automatically backed up by Learn

---
+++ src/createStore.js
@@ -0,0 +1,20 @@
+export default function createStore(reducer) {
+  let state
+
+  function getState() {
+    return state
+  }
+
+  function dispatch(action) {
+    state = reducer(state, action)
+    render()
+  }
+
+  dispatch({ type: '@@init' })
+
+  return { dispatch, getState}
+}
+
+function render() {
+  const container = document.getElementById('container');
+}
